6dc3297e0bb545ae3bc3b5c386c0257e31692ad2
M Nasir Khan <immnk97@gmail.com>
2025-02-09 00:39:38 +0500

    Add build output directory configuration to Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,30 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+  build: {
+    outDir: "dist", // This will set the output directory to "dist"
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+      },
+    },
+  },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+// 	plugins: [react()],
+// 	server: {
+// 		proxy: {
+// 			"/api": {
+// 				target: "http://localhost:5000",
+// 			},
+// 		},
+// 	},
+// });
